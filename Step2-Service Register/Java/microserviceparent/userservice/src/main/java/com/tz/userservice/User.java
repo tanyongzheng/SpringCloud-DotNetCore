1d6ed2c2e94c5f795e8ba5139bae764dc56d3b75
tanyongzheng <tanyongzheng@126.com>
2018-08-02 18:30:49 +0800

    创建用户服务，并注册到Eureka Server

---
+++ Step2-Service Register/Java/microserviceparent/userservice/src/main/java/com/tz/userservice/User.java
@@ -0,0 +1,34 @@
+package com.tz.userservice;
+
+public class User {
+
+    private String name;
+
+    private int age;
+
+    private boolean isDeleted;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
+    }
+}
