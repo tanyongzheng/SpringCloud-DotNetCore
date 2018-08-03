0a94d87971170ac79a711080d4b44dd3b6ead7b6
tanyongzheng <tanyongzheng@126.com>
2018-08-03 23:03:16 +0800

    .net core版服务发现与调用

---
+++ Step3-Service Discovery and Call/DotNetCore/MicroService/OrderService/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderService.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class OrderController
+    {
+        [Route("getalluser")]
+        [HttpGet]
+        public List<User> getAll()
+        {
+            List < User > list= null;
+            return list;
+        }
+
+    }
+}
---
+++ Step3-Service Discovery and Call/DotNetCore/MicroService/OrderService/Controllers/UserService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Steeltoe.Common.Discovery;
+
+namespace OrderService.Controllers
+{
+
+    public class UserService //: IFortuneService
+    {
+        DiscoveryHttpClientHandler _handler;
+        private const string serviceUrl = "http://fortuneService/api/fortunes/random";
+        public UserService(IDiscoveryClient client)
+        {
+            _handler = new DiscoveryHttpClientHandler(client);
+        }
+        public async Task<string> getAll()
+        {
+            var client = GetClient();
+            return await client.GetStringAsync(serviceUrl+"/getall");
+        }
+        private HttpClient GetClient()
+        {
+            var client = new HttpClient(_handler, false);
+            return client;
+        }
+    }
+}
