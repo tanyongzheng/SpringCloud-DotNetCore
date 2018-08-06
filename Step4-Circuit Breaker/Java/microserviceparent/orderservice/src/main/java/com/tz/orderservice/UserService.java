a74d144c6752491d8b3d3a52eb48ca31cc05527f
tanyongzheng <tanyongzheng@126.com>
2018-08-06 19:40:31 +0800

    java版断路器

--- Step4-Circuit Breaker/Java/microserviceparent/orderservice/src/main/java/com/tz/orderservice/UserService.java
+++ Step4-Circuit Breaker/Java/microserviceparent/orderservice/src/main/java/com/tz/orderservice/UserService.java
@@ -1,5 +1,6 @@
 package com.tz.orderservice;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -18,6 +19,7 @@ public class UserService {
 
     private String serviceUrl="http://userservice/user";
 
+    @HystrixCommand(fallbackMethod = "getAllError")
     public List<User> getAll() {
         ParameterizedTypeReference<List<User>> responseType = new ParameterizedTypeReference<List<User>>(){};
         ResponseEntity<List<User>> resp = restTemplate.exchange(serviceUrl+"/getall",
@@ -26,6 +28,7 @@ public class UserService {
         return list;
     }
 
+    @HystrixCommand(fallbackMethod = "getPortError")
     public String getPort(){
         String msg = restTemplate.getForObject(serviceUrl+"/getport", String.class);
         return msg;
@@ -34,5 +37,14 @@ public class UserService {
     public User getByName(String name){
         User user = restTemplate.getForObject(serviceUrl+"/getbyname?name="+name, User.class);
         return user;
+    }
+
+    //getAll回退方法
+    public List<User> getAllError() {
+        return null;
+    }
+    //getPort回退方法
+    public String getPortError() {
+        return "userservice服务断开，getPort方法调用错误!";
     }
 }
