24780f7dfa8d6d23a2593c665257f0f02d7cb013
tanyongzheng <tanyongzheng@126.com>
2018-08-07 18:58:55 +0800

    模拟.net 客户端调用网关服务

---
+++ Step5-API Gateway/DotNetCore/MicroService/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userServiceUrl = "http://localhost:5551/userservice/user/";//通过网关地址来调用服务
+            var client = new HttpClient { BaseAddress = new Uri(userServiceUrl) };
+            HttpResponseMessage response = client.GetAsync("getall").Result;
+            string responseStr = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(responseStr);
+            Console.ReadKey();
+        }
+    }
+}
